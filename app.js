d1a729d2d9b69324be33985fe1b8f21b217e0170
TheatreKnitter <ecolemansound@gmail.com>
2016-09-05 16:11:22 -0400

    fix layout

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 for (var i =1; i<101; i++){
 	if (i%3 === 0 && i%5 === 0) {
-		$("Body").append(document.write("fizzbuzz"))
-		console.log("fizzbuzz")
+		$("Body").append(document.write("fizzbuzz" + "<br>"));
+		console.log("fizzbuzz");
 	}
 	else if (i%3 === 0) {
-		$("body").append(document.write("fizz"))
-		console.log("fizz")
+		$("body").append(document.write("fizz" + "<br>"));
+		console.log("fizz");
 	}
 	else if (i%5 === 0) {
-		$("body").append(document.write("buzz"))
-		console.log("buzz")
+		$("body").append(document.write("buzz" + "<br>"));
+		console.log("buzz");
 	}
 	else {
-		$("body").append(document.write(i)).after("<br/>")
+		$("body").append(document.write((i) + "<br/>"));
 		console.log(i);
 	}
 };
